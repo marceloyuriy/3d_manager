825fdd50f2acd8a8f029842009006da1b3846e03
Marcelo Yuri <marceloyuriy@gmail.com>
2026-03-10 14:26:37 -0300

    Fix: Corrigindo erro de importação no urls.py

--- core/urls.py
+++ core/urls.py
@@ -1,17 +1,9 @@
-import robots_txt
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings # Importa as configurações (settings.py)
-from django.conf.urls.static import static # Ajuda a servir arquivos de mídia
 from django.http import HttpResponse
 
 
-urlpatterns = [
-    path('@aeroriver_2026_hub/', admin.site.urls),
-    path('', include('fila.urls')),
-    path('robots.txt', robots_txt),
-]
-
+# Esta é uma função simples, não um módulo externo
 def robots_txt(request):
     lines = [
         "User-Agent: *",
@@ -19,6 +11,14 @@ def robots_txt(request):
     ]
     return HttpResponse("\n".join(lines), content_type="text/plain")
 
-# Isso é essencial para que os arquivos (STL/GCODE) possam ser baixados durante o desenvolvimento
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns = [
+    # A sua nova porta secreta
+    path('portal-gestor-aero-2026/', admin.site.urls),
+
+    # O robots.txt para esconder a porta dos motores de busca
+    path('robots.txt', robots_txt),
+
+    # A ligação com o resto do sistema
+    path('', include('fila.urls')),
+]
